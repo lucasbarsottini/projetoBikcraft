767e9feb8d8d78732df3d24692ee86588f0468c6
Lucas Barsottini <lucasfonseca011@hotmail.com>
2023-11-01 22:14:16 -0300

    script

---
+++ js/script.js
@@ -0,0 +1,43 @@
+// Ativar Links menu
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link) {
+  const url = window.location.href;
+  const href = link.href;
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
+}
+
+links.forEach(ativarLink);
+
+// Ativar itens orçamento
+
+const parametro = new URLSearchParams(location.search);
+
+function ativarP(parametro1) {
+  const elemento = document.getElementById(parametro1);
+
+  elemento.checked = true;
+}
+
+parametro.forEach(ativarP);
+
+// slide perguntas
+
+const botao = document.querySelectorAll("button");
+
+function mostrar(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+  resposta.classList.toggle("slide");
+  const ativa = resposta.classList.contains("slide");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function mostrarRespostas(btn) {
+  btn.addEventListener("click", mostrar);
+}
+
+botao.forEach(mostrarRespostas);
